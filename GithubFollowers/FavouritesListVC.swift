f57a73999fa90742dfc4001366e64b7bd2e4cee5
Greg Ross <gregandrewross79@gmail.com>
2022-08-29 14:40:22 +0100

    Initial Commit

---
+++ GithubFollowers/FavouritesListVC.swift
@@ -0,0 +1,21 @@
+//
+//  FavouritesListVC.swift
+//  GithubFollowers
+//
+//  Created by Greg Ross on 29/08/2022.
+//
+
+import UIKit
+
+class FavouritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+    
+
+
+
+}
---
+++ GithubFollowers/SearchVC.swift
@@ -0,0 +1,20 @@
+//
+//  SearchVC.swift
+//  GithubFollowers
+//
+//  Created by Greg Ross on 29/08/2022.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+
+    }
+
+
+}

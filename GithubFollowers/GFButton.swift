359e0c96bf208b5e83f9f9194584614c00c4c61e
Greg Ross <gregandrewross79@gmail.com>
2022-08-29 16:28:56 +0100

    - Finished UI design of search view

---
+++ GithubFollowers/GFButton.swift
@@ -0,0 +1,45 @@
+//
+//  GFButton.swift
+//  GithubFollowers
+//
+//  Created by Greg Ross on 29/08/2022.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    // Custom init with custom colour and title text
+    init(backgroundColor: UIColor, title: String){
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+        
+    }
+    
+    
+    
+    private func configure(){
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    
+    
+    // This handles init from storyboard
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+}

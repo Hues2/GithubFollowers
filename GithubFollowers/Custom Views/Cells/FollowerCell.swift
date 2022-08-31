e9dc70a2ce0dfa280eb82ecc9fca7454c9a3c261
Greg Ross <greg.ross@networldsports.com>
2022-08-31 17:14:17 +0100

    - Cell image data now downloads, and displays in the collectionview - Each image is cached if it is not already, to avoid excessive networking requests

--- GithubFollowers/Custom Views/Cells/FollowerCell.swift
+++ GithubFollowers/Custom Views/Cells/FollowerCell.swift
@@ -30,6 +30,7 @@ class FollowerCell: UICollectionViewCell {
     
     func set(follower: Follower){
         userNameLabel.text = follower.login
+        avatarImageView.downloadImage(from: follower.avatarUrl)
     }
     
     

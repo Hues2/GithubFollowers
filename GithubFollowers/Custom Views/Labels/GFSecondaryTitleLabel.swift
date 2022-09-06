c1505f126a25fe8a8e5be3932048a6b2de66517f
Greg Ross <greg.ross@networldsports.com>
2022-09-06 10:14:12 +0100

    - Started user info UI layout

---
+++ GithubFollowers/Custom Views/Labels/GFSecondaryTitleLabel.swift
@@ -0,0 +1,38 @@
+//
+//  GFSecondaryTitleLabel.swift
+//  GithubFollowers
+//
+//  Created by Greg Ross on 06/09/2022.
+//
+
+import UIKit
+
+class GFSecondaryTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // Custom init
+    init(fontSize: CGFloat){
+        super.init(frame: .zero)
+        font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+        configure()
+    }
+    
+    
+    
+    private func configure(){
+        textColor = .secondaryLabel
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
---
+++ GithubFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GithubFollowers
+//
+//  Created by Greg Ross on 06/09/2022.
+//
+
+import Foundation
+
+
+enum SFSymbols{
+    
+}
